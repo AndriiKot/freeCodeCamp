10e87b194d55913d9032dd5a6695744de8a112c0
AndriiKot <a_kocyuba@ukr.net>
2023-03-30 16:46:31 +0200

    Creation JS Lesson 6 cicle for of for in

---
+++ example1.js
@@ -0,0 +1,17 @@
+﻿const basket = [
+  { price: 1000 },
+  { price: 23   },
+  { price: 500  },
+  { price: 12   }
+];
+
+for (const item of basket) {
+ console.log(`Price: ${item.price}`);
+}
+
+/*
+Price: 1000
+Price: 23
+Price: 500
+Price: 12
+*/
