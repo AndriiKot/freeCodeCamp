7f2ce52d97ea7052d0078628ba964ddebd26e31e
AndriiKot <a_kocyuba@ukr.net>
2023-03-30 20:04:13 +0200

    cycle for i

---
+++ example4_2.js
@@ -0,0 +1,18 @@
+﻿const basket = {
+  Laptop: 1500,
+  Mouse : 25,
+  Keyboard: 100,
+  HDMI: 10
+};
+
+for (const [name, price] of Object.entries(basket)) {
+  console.log(`Price of ${name} is ${price}`);
+}
+
+/*
+Price of Laptop is 1500
+Price of Mouse is 25
+Price of Keyboard is 100
+Price of HDMI is 10
+*/
+
