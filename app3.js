f8ee24e04c752c026165e0036c448299687472ea
AndriiKot <a_kocyuba@ukr.net>
2023-04-12 15:00:58 +0200

    Add methods push and shift

---
+++ app3.js
@@ -0,0 +1,12 @@
+﻿const arr = ['str1','str2',11,-4,true,false,101];
+
+console.log(arr);               // ['str1','str2',11,-4,true,false,101]
+console.log(arr[arr.length-1]);  //   101
+console.log(arr.at(-1));        //   101
+
+console.log(arr.shift());        //   'str1'
+console.log(arr);                //   ['str2',11,-4,true,false,101]
+
+console.log(arr.push(55));       //   7
+console.log(arr);                // ['str2',11,-4,true,false,101,55]
+
