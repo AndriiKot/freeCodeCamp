fc499bb0c03520e0551deadc2a5e707d447e6273
AndriiKot <a_kocyuba@ukr.net>
2023-04-12 15:11:54 +0200

    Add method indexOf

--- app5.js
+++ app5.js
@@ -5,3 +5,5 @@ const arr3 = arr1.concat(arr2);
 
 console.log(arr3)     // [1,2,3,10,20,30]
 
+
+
---
+++ app6.js
@@ -0,0 +1,4 @@
+﻿const arr1 = ['string 1', 'string 2', 'string 3']
+
+console.log(arr1.indexOf(arr1.at(-1)))  // 2
+console.log(arr1.indexOf('string 2'))   // 1
