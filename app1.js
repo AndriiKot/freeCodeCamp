b033e4ef54b9e6846207ea51bd937ed4769178e5
AndriiKot <a_kocyuba@ukr.net>
2023-04-12 14:47:41 +0200

    Creation JS Lesson 8 (Array)

---
+++ app1.js
@@ -0,0 +1,13 @@
+﻿const arr = [];
+
+ arr.push(1,2,3,2,3,'dsf');
+
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[1]);
+
+arr[0] = 'Start'
+
+console.log(arr);
+
+
---
+++ app2.js
@@ -0,0 +1,8 @@
+﻿const matrix = [
+  [1,2,3],
+  [0,0,4],
+  [9,8,6]
+]
+
+console.log(matrix[2][0]);
+
