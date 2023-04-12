1823cd0b1f57e9d6a5d0e211c59338d0a639cb4f
AndriiKot <a_kocyuba@ukr.net>
2023-04-12 15:53:53 +0200

    Add method join

---
+++ app9.js
@@ -0,0 +1,9 @@
+﻿const arrName = ['Andrii','Roma','Dima'];
+const stringName = arrName.slice();
+
+console.log(arrName);             // ['Andrii','Roma','Dima']
+console.log(stringName.join());   // Andrii,Roma,Dima
+console.log(stringName.join(''))  // AndriiRomaDima
+console.log(stringName.join(' ')) // Andrii Roma Dima
+
+
