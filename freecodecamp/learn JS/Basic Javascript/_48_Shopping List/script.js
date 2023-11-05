ce012bb27e9720614e5ad50c9c72e2d7b9e5902f
AndriiKot <a_kocyuba@ukr.net>
2023-11-05 09:39:17 +0100

    continuation tutorial 'SANDBOX'

---
+++ freecodecamp/learn JS/Basic Javascript/_48_Shopping List/script.js
@@ -0,0 +1,10 @@
+﻿//There should be at least 5 sub-arrays in the list.
+
+const myList = [
+  ['string',2],
+  ['string',2],
+  ['string',2],
+  ['string',2],
+  ['string',2],
+];
+
---
+++ freecodecamp/learn JS/Basic Javascript/_52_Global Scope and Functions/script.js
@@ -0,0 +1,31 @@
+﻿//Using let or const, declare a global
+//variable named myGlobal outside of
+//any function. Initialize it with a value of 10.
+
+//Inside function fun1, assign 5 
+//to oopsGlobal without using the var, let or const keywords.
+
+// Declare the myGlobal variable below this line
+const myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal here
+
+}
+
+// Only change code above this line
+
+function fun2() {
+  let output = "";
+  oopsGlobal = 5;
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
+
+fun2();
+
