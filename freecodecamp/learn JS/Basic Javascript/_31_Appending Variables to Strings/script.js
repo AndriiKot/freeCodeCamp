190c45ae003deab0746f5a417c11cab24f4b67fb
AndriiKot <a_kocyuba@ukr.net>
2023-11-05 08:47:29 +0100

    continuation tutorial 'SANDBOX'

---
+++ freecodecamp/learn JS/Basic Javascript/_31_Appending Variables to Strings/script.js
@@ -0,0 +1,4 @@
+﻿// Change code below this line
+const someAdjective = "KOT";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
---
+++ freecodecamp/learn JS/Basic Javascript/_41_Access Array Data with Indexes/script.js
@@ -0,0 +1,8 @@
+﻿//The console.log(array[0]) prints 50, and data has the value 60.
+
+//Create a variable called myData and set 
+//it to equal the first value of myArray using bracket notation.
+const myArray = [50, 60, 70];
+
+const myData = myArray[0];
+
