e86e81d4fedf651bb5cd3333496a983ace3e3ecf
AndriiKot <a_kocyuba@ukr.net>
2024-02-27 00:14:04 +0100

    continuation of the basic js course

---
+++ freecodecamp/learn_JS/Basic_Javascript/_53_Multiply_Two_Decimals_with_JavaScript/script.js
@@ -0,0 +1,1 @@
+const product = 2.0 * 5.0;
---
+++ freecodecamp/learn_JS/Basic_Javascript/_56_Assignment_with_a_Returned_Value/script.js
@@ -0,0 +1,9 @@
+﻿// Setup
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+processed = processArg(7)
+// Only change code below this line
