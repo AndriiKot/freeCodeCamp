1210a6659f4853b6a3c2c1f44a94ed11bdb8b34e
AndriiKot <a_kocyuba@ukr.net>
2024-02-27 09:59:07 +0100

    basic js

---
+++ freecodecamp/learn_JS/Basic_Javascript/__test_ruby_script.rb
@@ -0,0 +1,12 @@
+﻿# List Folders
+dir = Dir.new('./')
+dir.each_child  do |folder| 
+    p folder
+    p folder.include?(' ')
+    
+    if folder.include?(' ') 
+        new_folder_name = folder.gsub(/[^a-zA-Z0-9\.-_]/, '')
+        p new_folder_name
+    end
+    gets
+end
