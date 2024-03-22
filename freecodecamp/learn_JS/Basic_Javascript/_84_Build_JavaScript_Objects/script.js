14415ffac4226ac38778a1970d5dc578fe26026b
AndriiKot <a_kocyuba@ukr.net>
2024-03-22 19:08:56 +0100

    js basic

---
+++ freecodecamp/learn_JS/Basic_Javascript/_84_Build_JavaScript_Objects/script.js
@@ -0,0 +1,8 @@
+﻿const myDog = {
+  // Only change code below this line
+  name: "Triffan",
+  legs: 4,
+  tails: 1,
+  friends: ['Andrii','Olesia','Tatiana','Petr'],
+  // Only change code above this line
+};
---
+++ freecodecamp/learn_JS/Basic_Javascript/_93_Manipulating_Complex_Objects/script.js
@@ -0,0 +1,16 @@
+﻿const myMusic = [
+  {
+    artist: "Billy Joel",
+    title: "Piano Man",
+    release_year: 1973,
+    formats: ["CD", "8T", "LP"],
+    gold: true,
+  },
+  {
+    artist: "Nightwish",
+    title: "Wishmaster",
+    release_year: 2000,
+    formats: ["CD", "8T", "LP"],
+    gold: false,
+  },
+];
