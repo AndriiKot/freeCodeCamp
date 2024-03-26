81660c0aeb4ce5a99e0483a45b6418bfd695b694
AndriiKot <a_kocyuba@ukr.net>
2024-03-27 00:49:34 +0100

    101, 102 solved exercise

---
+++ freecodecamp/learn_JS/Basic_Javascript/_101_Iterate_Through_An_Array_With_A_For_Loop/script.js
@@ -0,0 +1,13 @@
+﻿// Setup
+const myArr = [2, 3, 4, 5, 6];
+
+// Only change code below this line
+
+let total = 0;
+const len = myArr.length;
+
+for (let i = 0; i < len; i++) {
+  total += myArr[i];
+}
+
+console.log(total); // 20
