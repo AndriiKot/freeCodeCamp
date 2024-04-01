ad43acd4567542d9b860c0c1272604247bf759cc
AndriiKot <a_kocyuba@ukr.net>
2024-04-01 15:43:59 +0100

    15-16 tasks ES6

---
+++ freecodecamp/learn_JS/ES6/_16_Write_Concise_Object_Literal_Declarations_Using_Object_Property_Shorthand/script.js
@@ -0,0 +1,18 @@
+﻿const createPersonOLD = (name, age, gender) => {
+  // Only change code below this line
+  return {
+    name: name,
+    age: age,
+    gender: gender,
+  };
+  // Only change code above this line
+};
+
+const createPersonNEW = (name, age, gender) => ({
+  name,
+  age,
+  gender,
+});
+
+console.log(createPersonOLD("Zodiac Hasbro", 56, "male"));
+console.log(createPersonNEW("Erik Graf", 89, "male"));
