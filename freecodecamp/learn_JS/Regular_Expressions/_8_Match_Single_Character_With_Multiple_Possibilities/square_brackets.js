b9dfb25529de4d884918bef30dac72424c977b1f
AndriiKot <a_kocyuba@ukr.net>
2024-04-04 18:55:29 +0200

    5-10 tasks 'Regular Expressions'

---
+++ freecodecamp/learn_JS/Regular_Expressions/_8_Match_Single_Character_With_Multiple_Possibilities/square_brackets.js
@@ -0,0 +1,19 @@
+﻿let quoteSample =
+  "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+
+// !!! squere brackets !!!!
+let vowelRegex = /[aeiou]/gi;
+// !!!!
+
+let result = quoteSample.match(vowelRegex);
+
+console.log(result);
+
+// [
+//     'e', 'a', 'e', 'o', 'u', 'i',
+//     'e', 'a', 'o', 'e', 'o', 'e',
+//     'I', 'a', 'e', 'o', 'o', 'e',
+//     'i', 'o', 'e', 'o', 'i', 'e',
+//     'i'
+// ]
+  
