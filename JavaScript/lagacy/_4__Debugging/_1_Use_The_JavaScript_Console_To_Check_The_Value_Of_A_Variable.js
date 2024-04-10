5160051ba30295ba928f361c605aa01becdf8419
AndriiKot <a_kocyuba@ukr.net>
2024-04-10 13:47:49 +0200

    linter fixed

--- JavaScript/lagacy/_4__Debugging/_1_Use_The_JavaScript_Console_To_Check_The_Value_Of_A_Variable.js
+++ JavaScript/lagacy/_4__Debugging/_1_Use_The_JavaScript_Console_To_Check_The_Value_Of_A_Variable.js
@@ -4,6 +4,5 @@ let b = 1;
 a++;
 console.log(a);
 
-
 let sumAB = a + b;
 console.log(sumAB);
--- JavaScript/lagacy/_4__Debugging/_3_Use_Typeof_To_Check_The_Type_Of_A_Variable.js
+++ JavaScript/lagacy/_4__Debugging/_3_Use_Typeof_To_Check_The_Type_Of_A_Variable.js
@@ -1,15 +1,15 @@
-﻿// Add two console.log() statements to check the typeof 
+﻿// Add two console.log() statements to check the typeof
 // each of the two variables seven and three in the code.
 
 let seven = 7;
 let three = "3";
 console.log(seven + three);
 
-console.log(typeof(seven)); 
-console.log(typeof(three));
+console.log(typeof seven);
+console.log(typeof three);
 
-console.log(typeof true);   // boolean
-console.log(typeof "");     // string
-console.log(typeof 0);      // number
-console.log(typeof []);     // object
-console.log(typeof {});     // object
+console.log(typeof true); // boolean
+console.log(typeof ""); // string
+console.log(typeof 0); // number
+console.log(typeof []); // object
+console.log(typeof {}); // object
