bc3e31b2acb6e0772cf02166d35056ce200cb5bb
AndriiKot <a_kocyuba@ukr.net>
2024-04-10 13:45:21 +0200

    FINISHED Project 'Debugging'

---
+++ JavaScript/lagacy/_4__Debugging/_6_Catch_Mixed_Usage_Of_Single_And_Double_Quotes.js
@@ -0,0 +1,10 @@
+﻿// Fix the string so it either uses different quotes for the href value, 
+// or escape the existing ones. Keep the double quote marks around the entire string.
+
+let innerHtml = "<p>Click here to <a href='#Home'>return home</a></p>";
+console.log(innerHtml);
+
+
+const grouchoContraction = "I've had a perfectly wonderful evening, but this wasn't it.";
+const quoteInString = "Groucho Marx once said 'Quote me as saying I was mis-quoted.'";
+const uhOhGroucho = 'I\'ve had a perfectly wonderful evening, but this wasn\'t it.';
