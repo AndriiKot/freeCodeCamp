1b7ea11d07c41717e19be51a0b1ba47606b19bc5
AndriiKot <a_kocyuba@ukr.net>
2024-04-11 15:03:09 +0200

    CREATE NEW PROJECT 'Data Structures'

---
+++ JavaScript/_5__Basic_Data_Structures/_1__Use_An_Array_To_Store_A_Collection_Of_Data.js
@@ -0,0 +1,36 @@
+﻿let yourArray = ["string", 1, false, [], {}];
+
+// ONE DIMENSION ARRAY
+let simpleArray = ["one", 2, "three", true, false, undefined, null];
+console.log(simpleArray.length);
+
+// MULTI DIMENSION ARRAY
+let multiDimArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+console.log(multiDimArray[1][2]);
+
+let complexArray = [
+  [
+    {
+      one: 1,
+      two: 2,
+    },
+    {
+      three: 3,
+      four: 4,
+    },
+  ],
+  [
+    {
+      a: "a",
+      b: "b",
+    },
+    {
+      c: "c",
+      d: "d",
+    },
+  ],
+];
