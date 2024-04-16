28b06fb7bd8d773d0908ee01e6fc1e5993808784
AndriiKot <a_kocyuba@ukr.net>
2024-04-16 16:30:54 +0200

    Repeat a String Repeat a String

---
+++ JavaScript/_6__Algorithms/_7__Repeat_A_String_Repeat_A_String.js
@@ -0,0 +1,12 @@
+﻿function repeatStringNumTimes(str, num) {
+  let newStr = "";
+
+  while (num > 0) {
+    num--;
+    newStr += str;
+  }
+  console.log(newStr);
+  return newStr;
+}
+
+repeatStringNumTimes("abc", 3);
