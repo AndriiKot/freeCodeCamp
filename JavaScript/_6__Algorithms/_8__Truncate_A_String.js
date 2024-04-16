7c62bbf2cfb7653fbed6ea2f6654b34babe35294
AndriiKot <a_kocyuba@ukr.net>
2024-04-16 16:55:32 +0200

    Truncate a String

---
+++ JavaScript/_6__Algorithms/_8__Truncate_A_String.js
@@ -0,0 +1,11 @@
+﻿function truncateString(str, num) {
+  if (str.length <= num) {
+    console.log(str);
+    return str;
+  }
+  const newStr = str.substr(0, num) + "...";
+  console.log(newStr);
+  return newStr;
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 298);
