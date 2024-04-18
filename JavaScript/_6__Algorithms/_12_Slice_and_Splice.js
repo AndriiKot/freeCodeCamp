d359bb8d47f7f7b9a90477c397672a584a442ab6
AndriiKot <a_kocyuba@ukr.net>
2024-04-18 11:53:07 +0200

    JS inDev 'Algorithms' task 12 'Slic and Splice'

---
+++ JavaScript/_6__Algorithms/_12_Slice_and_Splice.js
@@ -0,0 +1,16 @@
+﻿// !!! Получать длину второго массива 
+// function frankenSplice(arr1, arr2, n) {
+//     const newArray = Array.from({length: arr1.length + arr2.length}, () => 0);
+//     newArray.forEach((element,i) => {   
+//         for(let j = 0; j < n; j++) {
+//             newArray[i + j] = arr2[i + j];   
+//             console.log(newArray)   
+//         }
+//         newArray[i + n] = arr1[i];
+//         console.log(newArray)
+//         console.log('step: ' + (i + 1));
+//     });
+//     // return newArray
+//   }
+  
+//   console.log(frankenSplice([1, 2, 3], [4, 5, 6], 2));
