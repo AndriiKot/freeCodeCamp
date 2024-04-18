ccf8321cdac35675b094b25f1a21f9b77e0d5818
AndriiKot <a_kocyuba@ukr.net>
2024-04-18 14:05:34 +0200

    JS 'Object Oriented Programming' task 6 'Use a Constructor'

---
+++ JavaScript/_7__Object_Oriented_Programming/_6__Use_a_Constructor.js
@@ -0,0 +1,11 @@
+﻿function Dog() {
+  this.name = "Rupert";
+  this.color = "brown";
+  this.numLegs = 4;
+}
+
+const hound = new Dog();
+console.log(hound);
+
+hound.name = "Chappa";
+console.log(hound);
