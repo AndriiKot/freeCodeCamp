a0f787665fdb65cfee02d8f2dcb0453552e857bb
AndriiKot <a_kocyuba@ukr.net>
2024-04-28 20:10:37 +0200

    ' JavaScript 'Functional Programming' myFilter  task 10'

--- JavaScript/_8__Functional_Programming/_10_Implement_filter.js
+++ JavaScript/_8__Functional_Programming/_10_Implement_filter.js
@@ -1,7 +1,17 @@
 ﻿Array.prototype.myFilter = function (callback) {
-  const newArray = [];
   // Only change code below this line
-
+  const newArray = [];
+  const len = this.length;
+  for (let i = 0; i < len; i++) {
+    if (callback(this[i], i, this)) {
+      newArray.push(this[i]);
+    }
+  }
   // Only change code above this line
   return newArray;
 };
+
+const a = [1, 2, 3, 4, 5];
+const b = a.myFilter((val) => val >= 3);
+
+console.log(b);  // [3, 4, 5]
