102c723c60e98a49ab7d0342b7679aa9056dd81b
AndriiKot <a_kocyuba@ukr.net>
2024-04-30 12:13:16 +0200

    JavaScript 'Fucntionl Programming' URL task

---
+++ JavaScript/_8__Functional_Programming/_21_Convert_Strings_to_URL_Slugs.js
@@ -0,0 +1,5 @@
+﻿// Only change code below this line
+function urlSlug(title) {
+    return title.trim().toLowerCase().split(/\s+/).join("-");
+}
+console.log(urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone"));
