5633fd9301000094002858af4f158d186633f520
AndriiKot <a_kocyuba@ukr.net>
2024-05-07 11:03:40 +0200

    JavaScript 'Fucntionl Programming' Convert Strings to URL Slugs

--- JavaScript/_8__Functional_Programming/_21_Convert_Strings_to_URL_Slugs.js
+++ JavaScript/_8__Functional_Programming/_21_Convert_Strings_to_URL_Slugs.js
@@ -1,5 +1,4 @@
-﻿// Only change code below this line
-function urlSlug(title) {
+﻿function urlSlug(title) {
     return title.trim().toLowerCase().split(/\s+/).join("-");
 }
 console.log(urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone"));
