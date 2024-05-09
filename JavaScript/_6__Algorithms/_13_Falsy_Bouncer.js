ea94920f50dbc33dee4a4d16e3059eedca02308c
AndriiKot <a_kocyuba@ukr.net>
2024-05-09 11:15:21 +0200

    JavaScript 'Algorithms' Falsy Bouncer

---
+++ JavaScript/_6__Algorithms/_13_Falsy_Bouncer.js
@@ -0,0 +1,13 @@
+﻿function bouncer(arr) {
+  const newArray = arr.reduce((accumulator, currentValue) => {
+    if (currentValue) {
+      accumulator.push(currentValue);
+    }
+    return accumulator;
+  }, []);
+  return newArray;
+}
+
+bouncer([7, "ate", "", false, 9]);
+
+console.log(bouncer([7, "ate", "", false, 9]));
