989cbb9665a6fa870036e71d0da5a96f3232f9a8
AndriiKot <a_kocyuba@ukr.net>
2024-05-13 22:15:55 +0200

    JavaScript 'Algorithms' !!! FINISH !!!

--- JavaScript/_6__Algorithms/_16_Chunky_Monkey.js
+++ JavaScript/_6__Algorithms/_16_Chunky_Monkey.js
@@ -1,7 +1,19 @@
-﻿// function chunkArrayInGroups(arr, size) {
-//   const newArray = [];
-  
-//   return arr;
-// }
+﻿function chunkArrayInGroups(arr, size) {
+  const newArray = [];
+  const len = arr.length;
+  let i = 0;
+  for (let i = 0; i < len; i += size) {
+    const testArray = [];
+    let step = 0;
+    while (size > step && i + step < len) {
+      testArray.push(arr[i + step]);
+      step++;
+    }
+    newArray.push(testArray);
+  }
 
-// chunkArrayInGroups(["a", "b", "c", "d"], 2);
+  return newArray;
+}
+
+// console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4));
