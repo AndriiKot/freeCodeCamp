d20feae0355549e67a68031b462e75f83bf774d6
AndriiKot <a_kocyuba@ukr.net>
2024-05-18 10:19:20 +0200

    JavaScript DOM and WebApi 'Pyramid Generator' testing while

---
+++ JavaScript__DOM__WebApi/__01__Pyramid_Generator/__077__step.js
@@ -0,0 +1,31 @@
+﻿const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return ' '.repeat(rowCount - rowNumber)
+         + character.repeat(2 * rowNumber - 1) + 
+	 ' '.repeat(rowCount - rowNumber);
+}
+// comments
+/*for (let i = 1; i <= count; i += 1) {
+  rows.push(padRow(i, count));
+}*/
+
+let result = '';
+
+for (const row of rows) {
+  result = result + "\n" + row;
+};
+
+console.log(result);
+
+let continueLoop = false;
+let done = 0;
+
+while (continueLoop) {
+  done++;
+};
+
+
+
