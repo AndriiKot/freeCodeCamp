46c52a0c540e593bcb233676422b341106d31dce
AndriiKot <a_kocyuba@ukr.net>
2024-05-25 23:05:36 +0200

    Legacy Responsive Web Design Challenges  ! update_all_project.rb added !

---
+++ update_all_projects.rb
@@ -0,0 +1,12 @@
+﻿projects = {
+ HTML_Basic: 'https://github.com/AndriiKot/HTML__Basic__FreeCodeCamp.git',
+ CSS_Basic: 'https://github.com/AndriiKot/CSS__Basic__FreeCodeCamp.git',
+}
+
+update = "git subtree pull --prefix "
+
+projects.each do |folder, git_repo| 
+    system(`#{update}#{folder} #{git_repo} main`)
+end
+
+system(`git push`)
