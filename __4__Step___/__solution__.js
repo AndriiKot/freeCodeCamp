86042edebd02e40e1a3e64d2161fde2a45d64874
AndriiKot <AndriiKot@users.noreply.github.com>
2025-02-25 09:37:22 +0100

    'Gradebook_App !  refactor: all steps !

--- __4__Step___/__solution__.js
+++ __4__Step___/__solution__.js
@@ -1,33 +1,3 @@
-function getAverage(scores) {
-  let sum = 0;
-
-  for (const score of scores) {
-    sum += score;
-  }
-
-  return sum / scores.length;
-}
-
-function getGrade(score) {
-  if (score === 100) {
-    return "A++";
-  } else if (score >= 90) {
-    return "A";
-  } else if (score >= 80) {
-    return "B";
-  } else if (score >= 70) {
-    return "C";
-  } else if (score >= 60) {
-    return "D";
-  } else {
-    return "F";
-  }
-}
-
-function hasPassingGrade(score) {
-  return getGrade(score) !== "F";
-}
-
 function studentMsg(totalScores, studentScore) {
   const average = getAverage(totalScores);
   const studentGrade = getGrade(studentScore);
@@ -35,6 +5,31 @@ function studentMsg(totalScores, studentScore) {
   return `Class average: ${average}. Your grade: ${studentGrade}. ${
     passed ? "You passed the course." : "You failed the course."
   }`;
+}
+
+function hasPassingGrade(score) {
+  return score > 59;
+}
+
+function getAverage(scores) {
+  return scores.reduce((a, b) => a + b, 0) / scores.length;
+}
+
+function getGrade(score) {
+  const grades = {
+    "A++": score > 99,
+    A: score > 89,
+    B: score > 79,
+    C: score > 69,
+    D: score > 59,
+    F: score <= 59,
+  };
+
+  for (const [grade, condition] of Object.entries(grades)) {
+    if (condition) {
+      return grade;
+    }
+  }
 }
 
 console.log(studentMsg([56, 23, 89, 42, 75, 11, 68, 34, 91, 19], 100));
