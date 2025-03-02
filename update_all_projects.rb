2d5acdc7c5c952befc109662a5e42e7394d8dbf8
AndriiKot <AndriiKot@users.noreply.github.com>
2025-03-02 08:15:05 +0100

    'JavaScript Algorithms and Data Structures' feate: add new three 'Random_Background_Color_Changer'

--- update_all_projects.rb
+++ update_all_projects.rb
@@ -2,6 +2,7 @@
     Role_Playing_Game: 'https://github.com/AndriiKot/JS__Role_Playing_Game__FreeCodeCamp.git',
     Pyramid_Generator: 'https://github.com/AndriiKot/JS__Pyramid_Generator__FreeCodeCamp.git',
     Gradebook_App: 'https://github.com/AndriiKot/JS__Gradebook_App__FreeCodeCamp.git',
+    Random_Background_Color_Changer: 'https://github.com/AndriiKot/JS__Random_Background_Color_Changer__freeCodeCamp.git',
 }
 
 
